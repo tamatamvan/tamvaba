37a2ba569f8f178c0a2beeb55378d47da62222d0
Septian A Tama <sfirst.tama@gmail.com>
2018-01-31 16:43:26 +0700

    styling headline and summary styling

--- components/ArticleCard.vue
+++ components/ArticleCard.vue
@@ -1,17 +1,20 @@
 <template>
   <v-flex xs12 sm8 md6 class="gutter-8">
     <v-card>
-      <v-card-media src="/static/doc-images/cards/desert.jpg" height="200px">
+      <v-card-media src="https://static.pexels.com/photos/290164/pexels-photo-290164.jpeg" height="200px">
       </v-card-media>
       <v-card-title primary-title>
         <div>
           <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-          <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+          <p class="summary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis itaque maxime laboriosam asperiores error quibusdam nemo consequuntur alias placeat nisi quas illum blanditiis, cupiditate consequatur quis delectus dolores, et fugiat.
+          </p>
         </div>
       </v-card-title>
       <v-card-actions>
-        <v-btn flat color="orange">Share</v-btn>
-        <v-btn flat color="orange">Explore</v-btn>
+        <v-btn flat color="orange">Preview</v-btn>
+        <v-btn flat color="orange">Edit</v-btn>
+        <v-btn flat color="orange">Delete</v-btn>
       </v-card-actions>
     </v-card>
   </v-flex>
@@ -20,5 +23,13 @@
 <style scoped>
 .gutter-8 {
   padding: 8px;
+}
+.headline {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+.summary {
+  margin: 8px 0;
 }
 </style>
