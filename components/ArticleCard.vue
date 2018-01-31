56280aaaf96cfb15e76048749b2eb1cb6e7e0d23
Septian A Tama <sfirst.tama@gmail.com>
2018-01-31 16:24:51 +0700

    set container to fluid

---
+++ components/ArticleCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-flex xs12 sm8 md6 class="gutter-8">
+    <v-card>
+      <v-card-media src="/static/doc-images/cards/desert.jpg" height="200px">
+      </v-card-media>
+      <v-card-title primary-title>
+        <div>
+          <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+          <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+        </div>
+      </v-card-title>
+      <v-card-actions>
+        <v-btn flat color="orange">Share</v-btn>
+        <v-btn flat color="orange">Explore</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-flex>
+</template>
+
+<style scoped>
+.gutter-8 {
+  padding: 8px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -54,7 +54,7 @@
       </v-btn>
     </v-toolbar>
     <v-content>
-      <v-container>
+      <v-container fluid>
         <nuxt />
       </v-container>
     </v-content>
