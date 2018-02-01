e225cc1bee75b1dfcea786c42e04b09f1237e528
Septian A Tama <sfirst.tama@gmail.com>
2018-02-01 21:24:21 +0700

    mutating state when login success

--- store/index.js
+++ store/index.js
@@ -1,4 +1,35 @@
+import firebase from 'firebase'
+
 export const state = () => ({
   isLogin: false,
+  loginLoading: false,
+  errLogin: {
+    status: false,
+    message: ''
+  },
   userData: {}
 })
+
+export const mutations = {
+  LOGIN_SUCCESS (state, userData) {
+    state.userData = userData
+  }
+
+}
+
+export const actions = {
+  signInWithEmail ({ commit }, account) {
+    commit('LOGIN_PENDING')
+    return firebase.auth()
+      .signInWithEmailAndPassword(account.email, account.password)
+      .then(user => {
+        commit('LOGIN_SUCCESS', {
+          email: user.email,
+          displayName: user.displayName
+        })
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+}
