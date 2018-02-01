00634fca594f35de263498714f9cd7f360cbf629
Septian A Tama <sfirst.tama@gmail.com>
2018-02-01 21:25:03 +0700

    deleting separated buttonloading component

--- pages/login.vue
+++ pages/login.vue
@@ -2,46 +2,51 @@
   <v-layout align-center justify-center>
     <v-flex xs12 sm8 md6 text-xs-center>
       <h1>Login</h1>
-      <v-form v-model="valid">
+      <v-form v-model="valid" @submit.prevent="login(formLogin  )">
         <v-text-field
-          label="Name"
-          v-model="name"
-          :rules="nameRules"
-          :counter="10"
+          label="E-mail"
+          v-model="formLogin.email"
+          :rules="emailRules"
           required
-      ></v-text-field>
-      <v-text-field
-        label="E-mail"
-        v-model="email"
-        :rules="emailRules"
-        required
-      ></v-text-field>
-      <button-loading :btnValue="'Login'"></button-loading>
+        ></v-text-field>
+        <v-text-field
+          type="password"
+          label="Password"
+          v-model="formLogin.password"
+          :rules="passwdRules"
+          :counter="8"
+          required
+        ></v-text-field>
+        <v-btn type="submit">Login</v-btn>
       </v-form>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import ButtonLoading from '../components/ButtonLoading'
+import { mapActions } from 'vuex'
 export default {
-  components: {
-    ButtonLoading
-  },
   data () {
     return {
+      formLogin: {
+        email: '',
+        password: ''
+      },
       valid: false,
-      name: '',
-      nameRules: [
-        (v) => !!v || 'Name is required',
-        (v) => v.length <= 10 || 'Name must be less than 10 characters'
+      passwdRules: [
+        (v) => !!v || 'Password is required',
+        (v) => v.length >= 8 || 'Password must be more than 8 characters'
       ],
-      email: '',
       emailRules: [
         (v) => !!v || 'E-mail is required',
         (v) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/.test(v) || 'E-mail must be valid'
       ]
     }
+  },
+  methods: {
+    ...mapActions({
+      login: 'signInWithEmail'
+    })
   }
 }
 </script>
