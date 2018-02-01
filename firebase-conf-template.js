87ea556ae47248e2aa6a6e04794a7ac079b20c06
Septian A Tama <sfirst.tama@gmail.com>
2018-02-01 17:32:48 +0700

    add template for firebase-conf

---
+++ firebase-conf-template.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: '<YOUR FIREBASE API KEY>',
+  authDomain: '<YOUR AUTH DOMAIN>',
+  databaseURL: '<YOUR DATABASE URL>',
+  projectId: '<YOUR PROJECT ID>',
+  storageBucket: '<YOUR STORAGE BUCKET>',
+  messagingSenderId: '<YOUR MESSAGING ID>'
+}
+
+export default config
