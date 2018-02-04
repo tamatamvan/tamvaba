4efc3662a39351c20c2891502283a8fb6f517bef
Septian A Tama <sfirst.tama@gmail.com>
2018-02-04 20:49:14 +0700

    add mutations for async load article and post article

--- store/articles.js
+++ store/articles.js
@@ -10,12 +10,39 @@ const db = firebase.firestore()
 
 export const state = () => ({
   all: [],
+  err: {
+    status: false,
+    message: ''
+  },
   loading: false
 })
 
 export const mutations = {
   LOAD_ALL_ARTICLES_SUCCESS (state, articles) {
     state.all = articles
+  },
+  LOAD_ALL_ARTICLE_PENDING (state) {
+    state.loading = true
+  },
+  LOAD_ALL_ARTICLE_FAIL (state, errMsg) {
+    state.loading = false
+    state.err.status = true
+    state.err.loading = true
+  },
+  POST_ARTICLE_SUCCESS (state, article) {
+    state.all.push(article)
+    state.loading = false
+  },
+  POST_ARTICLE_PENDING (state) {
+    state.loading = true
+  },
+  POST_ARTICLE_FAIL (state, errMsg) {
+    state.err.status = true
+    state.err.message = errMsg
+    state.loading = false
+  },
+  RESET_ARTICLE_ERROR (state) {
+    state.article.err = {} 
   }
 }
 
