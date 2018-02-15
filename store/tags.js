60445641a515be81d406fda5bd9a2b4395be11e2
Septian A Tama <sfirst.tama@gmail.com>
2018-02-15 11:42:52 +0700

    creating actions-mutations for add new tag

--- store/tags.js
+++ store/tags.js
@@ -1,4 +1,5 @@
 import db from '~/utils/initFirestore'
+import slug from 'slug'
 
 export const state = () => ({
   all: [],
@@ -12,6 +13,10 @@ export const state = () => ({
 export const mutations = {
   LOAD_ALL_TAGS_SUCCESS (state, tags) {
     state.all = tags
+    state.isLoading = false
+  },
+  ADD_TAG_SUCCESS (state, tag) {
+    state.all.push(tag)
     state.isLoading = false
   },
   SET_ERROR (state, errMsg) {
@@ -36,6 +41,22 @@ export const actions = {
         commit('LOAD_ALL_TAGS_SUCCESS', tags)
       })
       .catch(err => commit('SET_ERROR', err.message))
+  },
+  addNew ({ commit }, tagName) {
+    const tagId = slug(tagName).toLowerCase()
+    db.collection('tags').doc(tagId)
+      .set({
+        tag_name: tagName
+      })
+      .then(() => {
+        commit('ADD_TAG_SUCCESS', {
+          id: tagId,
+          tag_name: tagName
+        })
+      })
+      .catch(err => {
+        commit('SET_ERROR', err.message)
+      })
   }
 }
 
